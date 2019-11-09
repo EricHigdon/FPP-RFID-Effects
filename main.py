b8847f831ff4bb532946babc9e6a4e835c4a4e05
Falcon Player <fpp@FPP>
2019-11-09 00:39:04 +0000

    added encryption to the profile ID

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
+from passlib.hash import argon2
 
-DEFAULT_EFFECT = 'purple'
-PROFILES = {
-	'Eric': {
-		'effect': 'blue'
-	}
-}
+EFFECTS = [
+	'blue',
+	'purple'
+]
+
+DEFAULT_EFFECT = EFFECTS[0]
+PROFILES = {}
 
 def start_effect(effect):
 	cmd = 'fpp -e {},1,1'.format(effect)
@@ -14,19 +16,34 @@ def start_effect(effect):
 def kill_effect(effect):
 	cmd = 'fpp -E {}'.format(effect)
 	os.system(cmd)
-
-def create_user(id):
 	
+def get_profile(id):
+	for key, profile in PROFILES.items():
+		if argon2.verify(id, key):
+			print('Loading profile: {}'.format(profile['name']))
+			return profile
+	return None
+
+def create_profile(id):
+	id = argon2.hash(id)
+	name = input('Enter the new user\'s name: ')
+	print('Available effects:')
+	for effect in EFFECTS:
+		print(effect)
+	effect = input('Choose an effect for the new user: ')
+	PROFILES[id] = {
+		'name': name,
+		'effect': effect
+	}
 
 def main():
 	old_effect = None
 	try:
-		for profile in PROFILES.values():
-			kill_effect(profile.get('effect', DEFAULT_EFFECT))
-		kill_effect(DEFAULT_EFFECT)
+		for effect in EFFECTS:
+			kill_effect(effect)
 		while True:
 			id = input('ID: ')
-			profile = PROFILES.get(id, None)
+			profile = get_profile(id)
 			if profile is not None:
 				effect = profile.get('effect', DEFAULT_EFFECT)
 				if old_effect is not None:
@@ -35,9 +52,8 @@ def main():
 				old_effect = effect
 			else:
 				print('No user with that ID extists')
-				new_user = input('Would you like to create a new user (y/n)?')
-				if new_user == 'y':
-					create_user(id)
+				if input('Would you like to create a new user (y/n)? ').lower() == 'y':
+					create_profile(id)
 	except KeyboardInterrupt:
 		print('\nExiting...')
 	finally:
